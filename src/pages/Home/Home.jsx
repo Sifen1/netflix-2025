62e4f5a083eaa218620e4e86504ae681b0b2657e
sifen <sifaman4@gmail.com>
2025-02-18 21:56:51 +0300

    ROWS

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,8 +9,8 @@ function Home() {
   return (
     <>
         <Header />
-        {/* <Banner /> 
-        <RowList /> */}
+        {/* <Banner />  */}
+        <RowList />
         <Footer />
     </>
   )
